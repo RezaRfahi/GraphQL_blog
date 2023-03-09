d84fc5b58b95836d7f20c11dfd3392a05c8e9c00
RezaRfahi <refahireza82@gmail.com>
2023-03-09 22:51:20 +0330

    modify DatabaseSeeder

--- QL_Blog/database/seeders/DatabaseSeeder.php
+++ QL_Blog/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Article;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-         \App\Models\User::factory(10)->create();
+         \App\Models\User::factory(10)->hasArticles(3)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
