b5cbbd78b8551165b6f5ee267ffbcc8a3bd6c921
RezaRfahi <refahireza82@gmail.com>
2023-03-09 22:34:29 +0330

    set relationship in Models

--- QL_Blog/app/Models/Article.php
+++ QL_Blog/app/Models/Article.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title', 'body'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
