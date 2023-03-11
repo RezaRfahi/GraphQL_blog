695564f9ff8b5e448a18ca4b0ad3e8617e2fd9dc
RezaRfahi <refahireza82@gmail.com>
2023-03-11 23:01:12 +0330

    config comment type

--- QL_Blog/app/GraphQL/Types/CommentType.php
+++ QL_Blog/app/GraphQL/Types/CommentType.php
@@ -4,19 +4,27 @@ declare(strict_types=1);
 
 namespace App\GraphQL\Types;
 
+use GraphQL\Type\Definition\Type;
 use Rebing\GraphQL\Support\Type as GraphQLType;
 
 class CommentType extends GraphQLType
 {
     protected $attributes = [
         'name' => 'Comment',
-        'description' => 'A type'
+        'description' => 'A type of Comment'
     ];
 
     public function fields(): array
     {
         return [
-
+            'body' => [
+                'type' => Type::string(),
+                'description' => 'context of the Comment'
+            ],
+            'approval' => [
+                'type' => Type::boolean(),
+                'description' => 'Show this comment or No'
+            ]
         ];
     }
 }
