b8a7d4a29d273de7ef61f93dc5ab76ae271c5cdf
RezaRfahi <refahireza82@gmail.com>
2023-03-11 22:44:00 +0330

    set comment Factory

--- QL_Blog/database/factories/CommentFactory.php
+++ QL_Blog/database/factories/CommentFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Article;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +19,10 @@ class CommentFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'body' => $this->faker->paragraph(),
+            'article_id' => Article::all()->random()->id,
+            'user_id' => User::all()->random()->id,
+            'approval' => $this->faker->boolean(40)
         ];
     }
 }
