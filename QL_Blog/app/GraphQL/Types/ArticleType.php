7045cd4644bfba7ed5f897a87b89c1ca4434d57a
RezaRfahi <refahireza82@gmail.com>
2023-03-11 13:26:52 +0330

    config articleType successfully

--- QL_Blog/app/GraphQL/Types/ArticleType.php
+++ QL_Blog/app/GraphQL/Types/ArticleType.php
@@ -4,19 +4,34 @@ declare(strict_types=1);
 
 namespace App\GraphQL\Types;
 
-use Rebing\GraphQL\Support\Type as GraphQLType;
+use GraphQL\Type\Definition\Type;
 
 class ArticleType extends GraphQLType
 {
     protected $attributes = [
         'name' => 'Article',
-        'description' => 'A type'
+        'description' => 'A type of article'
     ];
 
     public function fields(): array
     {
         return [
-
+            'title' => [
+                'type' => Type::string(),
+                'description' => 'the title of article'
+            ],
+            'body' => [
+                'type' => Type::string(),
+                'description' => 'article context'
+            ],
+            'user_id' => [
+                'type' => Type::int(),
+                'description' => 'article creator identity'
+            ],
+            'like_count' => [
+                'type' => Type::int(),
+                'description' => 'count of likes'
+            ]
         ];
     }
 }
