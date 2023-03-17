53f5dd3afa89fa55f9b7afb59347966d7ebd83e1
RezaRfahi <refahireza82@gmail.com>
2023-03-17 21:09:13 +0330

    config RegisterUser mutation

--- QL_Blog/app/GraphQL/Mutations/Auth/RegisterUser.php
+++ QL_Blog/app/GraphQL/Mutations/Auth/RegisterUser.php
@@ -7,25 +7,36 @@ namespace App\GraphQL\Mutations\Auth;
 use Closure;
 use GraphQL\Type\Definition\ResolveInfo;
 use GraphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Facades\GraphQL;
 use Rebing\GraphQL\Support\Mutation;
 use Rebing\GraphQL\Support\SelectFields;
 
 class RegisterUser extends Mutation
 {
     protected $attributes = [
-        'name' => 'auth/RegisterUser',
-        'description' => 'A mutation'
+        'name' => 'RegisterUser',
+        'description' => 'A mutation for Register new User'
     ];
 
     public function type(): Type
     {
-        return Type::listOf(Type::string());
+        return GraphQL::type('User');
     }
 
     public function args(): array
     {
         return [
-
+            'name' => [
+                'type' => Type::string(),
+                'description' => 'the Name of user'
+            ],
+            'email' => [
+                'type' => Type::string(),
+                'description' => 'the email of user'
+            ],
+            'password' => [
+                'type' => Type::string(),
+            ]
         ];
     }
 
